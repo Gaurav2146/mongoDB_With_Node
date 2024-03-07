51622107b5affcf881e21107ae4cd2db865e4d1e
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2024-03-07 12:33:35 +0530

    changes done

--- dist/index.js
+++ dist/index.js
@@ -19,30 +19,33 @@ const filter = {};
 const projection = {};
 const sort = {};
 const collation = {};
-app.get("/", (req, res, next) => {
-    fetchData()
-        .then((result) => {
-        res.status(200).json(result);
-    })
-        .catch((error) => {
-        res.status(400).json({ error: error });
+mongodb_1.MongoClient.connect("mongodb://localhost:27017/").then((client) => {
+    app.get("/", (req, res, next) => {
+        fetchData()
+            .then((result) => {
+            res.status(200).json(result);
+        })
+            .catch((error) => {
+            res.status(400).json({ error: error });
+        });
     });
-});
-async function fetchData() {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const client = await mongodb_1.MongoClient.connect("mongodb://localhost:27017/");
-            const coll = client.db("Test").collection("user");
-            const cursor = coll.find(filter, { projection, sort, collation });
-            const result = await cursor.toArray();
-            await client.close();
-            resolve(result);
-        }
-        catch (error) {
-            reject(error);
-        }
+    async function fetchData() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const coll = client.db("Test").collection("user");
+                const cursor = coll.find(filter, { projection, sort, collation });
+                const result = await cursor.toArray();
+                await client.close();
+                resolve(result);
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    app.listen(4500, () => {
+        console.log("Port is listening on port 4500");
     });
-}
-app.listen(4500, () => {
-    console.log("Port is listening on port 4500");
+}).catch((error) => {
+    console.log(error);
 });
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import bodyParser from "body-parser";
 app.use(bodyParser.json());
 
 // Middleware for parsing URL-encoded bodies
-app.use(bodyParser.urlencoded({ extended: true,limit:1024 }));
+app.use(bodyParser.urlencoded({ extended: true, limit: 1024 }));
 /*
  * Requires the MongoDB Node.js Driver
  * https://mongodb.github.io/node-mongodb-native
@@ -19,31 +19,36 @@ const projection: any = {};
 const sort: any = {};
 const collation: any = {};
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  fetchData()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((error) => {
-      res.status(400).json({ error: error });
+MongoClient.connect("mongodb://localhost:27017/")
+  .then((client) => {
+    app.get("/", (req: Request, res: Response, next: NextFunction) => {
+      fetchData()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((error) => {
+          res.status(400).json({ error: error });
+        });
     });
-});
 
-async function fetchData() {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const client = await MongoClient.connect("mongodb://localhost:27017/");
-      const coll = client.db("Test").collection("user");
-      const cursor = coll.find(filter, { projection, sort, collation });
-      const result = await cursor.toArray();
-      await client.close();
-      resolve(result);
-    } catch (error) {
-      reject(error);
+    async function fetchData() {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const coll = client.db("Test").collection("user");
+          const cursor = coll.find(filter, { projection, sort, collation });
+          const result = await cursor.toArray();
+          await client.close();
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
     }
-  });
-}
 
-app.listen(4500, () => {
-  console.log("Port is listening on port 4500");
-});
+    app.listen(4500, () => {
+      console.log("Port is listening on port 4500");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
